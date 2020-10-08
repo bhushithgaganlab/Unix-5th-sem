bdaec36c8548507d11a759d5d3cd282514d85ff7
bhushithgaganlab <42802614+bhushithgaganlab@users.noreply.github.com>
2020-10-08 15:27:14 +0530

    Week - 3

---
+++ Week-3/argumentsPassing.sh
@@ -0,0 +1,9 @@
+if test $# -eq 0
+then
+echo "Usage $0 pattern file">/dev/tty
+elif test $# -eq 2
+then
+grep "$1" $2 || echo "$1 not found in $2">/dev/tty
+else
+echo "No two arguments">dev/tty
+fi
